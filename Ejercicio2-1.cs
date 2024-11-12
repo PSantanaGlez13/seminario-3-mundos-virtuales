1d538e758cd74d2e9a42d105b37c2f41930be751
Pablo Santana González <91564638+PSantanaGlez13@users.noreply.github.com>
2024-11-12 15:39:24 +0000

    Add files via upload

---
+++ Ejercicio2-1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(2f, 2f, 2f);
+        transform.Rotate(0f, 30f, 0f);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ejercicio2-2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0f, 30f, 0f);
+        transform.Translate(2f, 2f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
